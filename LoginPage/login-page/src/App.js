c33cf0e97dfbb04d4fbce75a321452b8b8e79b13
PrompipatJa <prompipat18348@gmail.com>
2024-01-15 01:12:59 +0700

    redesign login page add logo & animation

--- LoginPage/login-page/src/App.js
+++ LoginPage/login-page/src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import { RiUser5Fill } from "react-icons/ri";
 import { FaUnlockAlt } from "react-icons/fa";
+import { LuLogIn } from "react-icons/lu";
+import { GiBookmarklet } from "react-icons/gi";
 
 function App() {
   return (
@@ -10,19 +12,21 @@ function App() {
         <div id="stars"></div>
         <div id="stars2"></div>
         <div id="stars3"></div>
-          <form action="">
-              <h1>Code4Dad</h1>
-              <div className='input-box'>
-                  <input type="text" placeholder='Username' required/>
-                  <RiUser5Fill className='icon' />
-              </div>
-              <div className='input-box'>
-                  <input type="password" placeholder='Password' required/>
-                  <FaUnlockAlt className='icon' />
-              </div>
+        <div className='loginlogo'>Login<LuLogIn /></div>
+        <form action="">
+          <div className='mainlogo'><GiBookmarklet /></div>
+          <h1>Code4Dad</h1>
+          <div className='input-box'>
+              <input type="text" placeholder='Username' required/>
+              <RiUser5Fill className='icon' />
+          </div>
+          <div className='input-box'>
+              <input type="password" placeholder='Password' required/>
+              <FaUnlockAlt className='icon' />
+          </div>
               
-              <button type="submit">Login</button>
-          </form>
+          <button type="submit">Login</button>
+        </form>
       </div>
 
     </div>
